90a5284e4090db4bc7242b8c5da1aa7f61bd4ddf
sS2S2bin <3dltnqls@naver.com>
2024-05-23 11:11:56 +0900

    guestbook2 init

---
+++ guestbook2/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,33 @@
+package guestbook.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("a");
+		
+		if("form".equals(action)) {
+			
+		}else if("delete".equals(action)) {
+			
+		}else {
+			/*default action (list)*/
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ guestbook2/src/main/webapp/WEB-INF/views/delete.jsp
@@ -0,0 +1,22 @@
+<%@page import="guestbook.dao.GuestbookDao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%
+	request.setCharacterEncoding("utf-8");
+	String no = request.getParameter("no");
+	String password = request.getParameter("password");
+	
+
+	new GuestbookDao().deleteByNo(Long.parseLong(no),password);
+	response.sendRedirect("/guestbook1");
+%>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+</body>
+</html>

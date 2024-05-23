80408200114fdca4223c1de6f7f60b53aeca4408
sS2S2bin <3dltnqls@naver.com>
2024-05-23 11:11:42 +0900

    emaillist2

--- emaillist2/src/main/java/emaillist/controller/EmaillistServlet.java
+++ emaillist2/src/main/java/emaillist/controller/EmaillistServlet.java
@@ -1,13 +1,16 @@
 package emaillist.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import emaillist.dao.EmaillistDao;
+import emaillist.vo.EmaillistVo;
 
 
 public class EmaillistServlet extends HttpServlet {
@@ -17,13 +20,32 @@ public class EmaillistServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("a");
-		if("".equals(action)) {
+		
+		if("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
 			
-		}else if("".equals(action)) {
+		}else if("add".equals(action)) {
+			request.setCharacterEncoding("utf-8");
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
+			
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+			
+			new EmaillistDao().insert(vo);
+			
+			response.sendRedirect(request.getContextPath()+"/el");
 			
 		}else {
 			/* default action ( list) */
-			new EmaillistDao().findAll();
+			List<EmaillistVo> list = new EmaillistDao().findAll();
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
 		}
 		
 		

a90d324689e8b5611b31316d5e0a9650bee9f52a
sS2S2bin <3dltnqls@naver.com>
2024-05-23 10:06:07 +0900

    emaillist2 init

---
+++ emaillist2/src/main/java/emaillist/controller/EmaillistServlet.java
@@ -0,0 +1,37 @@
+package emaillist.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import emaillist.dao.EmaillistDao;
+
+
+public class EmaillistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String action = request.getParameter("a");
+		if("".equals(action)) {
+			
+		}else if("".equals(action)) {
+			
+		}else {
+			/* default action ( list) */
+			new EmaillistDao().findAll();
+		}
+		
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
